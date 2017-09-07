35f8f14b888b57e13dc71391e4abffc9375e4914
sanygus <sashagusarov@mail.ru>
2017-09-07 09:32:48 +0300

    sf

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const getPhoto = (deviceID) => {
   return new Promise((resolve, reject) => {
     request(`http://geoworks.pro:3000/${deviceID}/photo`, {encoding: 'binary'}, (error, resp, body) => {
       if (resp.headers['content-type'] === 'image/jpeg') {
-        fs.writeFile(`${__dirname}/../app-new-web/static/photos/${deviceID}/${moment().format('YYYY-MM-DDTHH-mm')}.jpg`, body, 'binary', (err) => {
+        fs.writeFile(`${__dirname}/../app-new-web/static/photos/${deviceID}/${moment().format('YYYY-MM-DD-HH-mm-ss')}.jpg`, body, 'binary', (err) => {
           //if (err && err.code === 'ENOENT') { }
           resolve();
         });
